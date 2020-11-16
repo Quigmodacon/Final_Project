4d5fc1bb304512658cd13b8e95d35b60c87d18d5
Zachary Law <zachlaw90@gmail.com>
2020-11-15 21:14:51 -0500

    11/15/2020 turn in

---
+++ Project/RRG_Prototyping/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage = 10;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Debug.Log("enemy hit!!, check bullet script");
+            other.GetComponent<EnemyHealth>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
